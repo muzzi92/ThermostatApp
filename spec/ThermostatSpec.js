75edcab4056f124963d9a3dce986cf2207e78494
Muzzi Aldean <amiraldean@gmail.com>
2018-05-16 10:19:23 +0100

    gave starting temp of 20

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+describe('Thermostat', function(){
+  var thermostat;
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  describe('temperature', function(){
+    it('starts with a temperature of 20', function(){
+      expect(thermostat.temperature.toEqual(20);
+    });
+  });
+});
