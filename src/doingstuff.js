506226cba8932bed1d4cb3a8fb97a5a85afc0fb8
Muzzi Aldean <amiraldean@gmail.com>
2018-05-16 15:08:25 +0100

    add jquery functionality to buttons

--- src/doingstuff.js
+++ src/doingstuff.js
@@ -6,4 +6,21 @@ $( document ).ready(function() {
         console.log(thermostat.temperature)
     });
 
+    $( "#temp").text(thermostat.temperature)
+
+    $( "#up" ).click(function(){
+      thermostat.up(1);
+      $("#temp").text(thermostat.temperature)
+    })
+
+    $( "#down" ).click(function(){
+      thermostat.down(1);
+      $("#temp").text(thermostat.temperature)
+    })
+
+    $( "#reset" ).click(function(){
+      thermostat.reset();
+      $("#temp").text(thermostat.temperature)
+    })
+
 });
